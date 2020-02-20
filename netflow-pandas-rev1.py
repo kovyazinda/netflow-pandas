aeb3739ffb94d6a2ae21f75d1fcaaaa8c1d6fecd
kovyazinda <kovyazinda@gmail.com>
2020-02-20 10:14:03 +0600

    Update netflow-pandas-rev1.py

--- netflow-pandas-rev1.py
+++ netflow-pandas-rev1.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[12]:
-
 
 # import Pandas and Numpy for data processing
 import pandas as pd
@@ -24,14 +22,8 @@ inputfile = 'netflow.csv'
 df = pd.read_csv(inputfile,sep = ',',decimal=".",error_bad_lines=False,low_memory=False)
 
 
-# In[13]:
-
-
 # showing first 5 rows - jupyter only
 df.head()
-
-
-# In[14]:
 
 
 # dimensions of data set
@@ -47,9 +39,6 @@ print("Data Frame Info:")
 print(df.info())
 
 
-# In[15]:
-
-
 #Grouping - slow
 #columns_to_show = ['Размер начислений','Объем трафика в МБ']
 #df.groupby(['Описание услуги']).sum()[columns_to_show]
@@ -61,76 +50,24 @@ print(df.info())
 #df.pivot_table(['Размер начислений','Объем трафика в МБ'],index=['Описание услуги'],aggfunc='sum', fill_value = 0, margins=True,margins_name= 'Grand Total')
 
 
-# In[ ]:
+#display gateways
+#pivot_gw=df.pivot_table(['nf_pkts'],index=['nf_ipv4_next_hop'],aggfunc='sum', fill_value = 0, margins=False)
+#pivot_gw
 
-
-
-
-
-# In[16]:
-
-
-#v1 - too much unstructured data with many to many connection
-#pivot=df.pivot_table(['nf_pkts','nf_bytes'],index=['nf_direction','nf_proto','nf_src_port','nf_dst_port','nf_dst_address','nf_src_address'],aggfunc='sum', fill_value = 0, margins=True,margins_name= 'Grand Total')
-#v2 - separate pivots for nf_src to nf_dst_address  and nf_dst to nf_src_address
-#pivot_src=df.pivot_table(['nf_pkts'],index=['nf_direction','nf_proto','nf_src','nf_dst_address'],aggfunc='sum', fill_value = 0, margins=False)
-#v3 - with filter by direction to filter ingress
-pivot_src=df[df.nf_ipv4_next_hop == "10.156.64.221"].pivot_table(['nf_pkts'],index=['nf_proto','nf_src','nf_dst_address'],aggfunc='sum', fill_value = 0, margins=False)
-
-
-# In[17]:
-
-
-pivot_src
-
-
-# In[18]:
-
-
-#v2
-#pivot_dst=df.pivot_table(['nf_pkts'],index=['nf_direction','nf_proto','nf_dst','nf_src_address'],aggfunc='sum', fill_value = 0, margins=False)
-#v3 - with filter for direction egress
-pivot_dst=df[df.nf_ipv4_next_hop == "10.130.251.190"].pivot_table(['nf_pkts'],index=['nf_proto','nf_dst','nf_src_address'],aggfunc='sum', fill_value = 0, margins=False)
-
-
-# In[19]:
-
-
-pivot_dst
-
-
-# In[20]:
-
-
-# Not working until Unicode decoding issue can be resovled
-#with pd.ExcelWriter('output.xlsx') as writer:
-# pivot.to_excel(writer)
-
-#plotting
-#small_table=df.pivot_table(['nf_pkts'],index=['nf_direction','nf_proto'],aggfunc='sum', fill_value = 0)
-#plt.figure(figsize=(20,10))
-#small_table.plot(kind='bar',figsize=(20,10))
-#pivot.plot();
-#pivot_src.plot(kind='bar',figsize=(20,10))
-
-
-# In[21]:
-
-
-
-#v1
-#pivot.to_csv('output_netflow.csv', sep=';', na_rep='', header=True, index=True,mode='w', encoding='utf8', compression='infer', quoting=None, quotechar='"', line_terminator=None, chunksize=None, date_format=None, doublequote=True, escapechar=None, decimal=',')
-pivot_src.to_csv('output_netflow_src.csv', sep=';', na_rep='', header=True, index=True,mode='w', encoding='utf8', compression='infer', quoting=None, quotechar='"', line_terminator=None, chunksize=None, date_format=None, doublequote=True, escapechar=None, decimal=',')
-pivot_dst.to_csv('output_netflow_dst.csv', sep=';', na_rep='', header=True, index=True,mode='w', encoding='utf8', compression='infer', quoting=None, quotechar='"', line_terminator=None, chunksize=None, date_format=None, doublequote=True, escapechar=None, decimal=',')
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
+#v4 - getting list of gateways and forming pivots by src and dst for each
+print("Found Gateways:")
+for gateway in df.nf_ipv4_next_hop.unique():
+    print(gateway)
+# generate pivots for each gateway src and dst
+    source_pivot_filename='netflow_src_'+gateway+'.csv'
+    destination_pivot_filename='netflow_dst_'+gateway+'.csv'
+    pivot_src=df[df.nf_ipv4_next_hop == gateway].pivot_table(['nf_pkts'],index=['nf_proto','nf_src','nf_dst_address'],aggfunc='sum', fill_value = 0, margins=False)
+    pivot_dst=df[df.nf_ipv4_next_hop == gateway].pivot_table(['nf_pkts'],index=['nf_proto','nf_dst','nf_src_address'],aggfunc='sum', fill_value = 0, margins=False)
+    print("writing:",source_pivot_filename)
+    pivot_src.to_csv(source_pivot_filename, sep=';', na_rep='', header=True, index=True,mode='w', encoding='utf8', compression='infer', quoting=None, quotechar='"', line_terminator=None, chunksize=None, date_format=None, doublequote=True, escapechar=None, decimal=',')
+#    pivot_src.to_csv(source_pivot_filename, sep=';', na_rep='', header=True, index=True,mode='w', encoding='utf8', quoting=None, quotechar='"', line_terminator=None, chunksize=None, date_format=None, doublequote=True, escapechar=None, decimal=',')
+    print("writing:",destination_pivot_filename)
+    pivot_dst.to_csv(destination_pivot_filename, sep=';', na_rep='', header=True, index=True,mode='w', encoding='utf8', compression='infer', quoting=None, quotechar='"', line_terminator=None, chunksize=None, date_format=None, doublequote=True, escapechar=None, decimal=',')
+#    pivot_dst.to_csv(destination_pivot_filename, sep=';', na_rep='', header=True, index=True,mode='w', encoding='utf8', quoting=None, quotechar='"', line_terminator=None, chunksize=None, date_format=None, doublequote=True, escapechar=None, decimal=',')
 
 
